1adbfe7858d7fc4426db1f86fb064383ec2eba48
Sarot Busala <sabuza97@gmail.com>
2018-04-29 16:30:01 +0700

    Adjust StudentPanel Buttons

--- src/StudentMenu/StudentPanel/index.js
+++ src/StudentMenu/StudentPanel/index.js
@@ -9,17 +9,19 @@ export default class StudentPanel extends Component {
         return (
             <div>
             <div> Menu Page (Student) </div>
-            <div><button className="btn btn-default" onClick={ () =>{ this.props.changeState({page:'StudentInformation'})}}>ข้อมูลนักศึกษา</button></div>
-            <div><button className="btn btn-default" onClick={ () =>{ this.props.changeState({page:'RegisterCourse'})}}>ลงทะเบียนรายวิชา</button></div>
-            <div><button className="btn btn-default" onClick={ () =>{ this.props.changeState({page:'ManageCourse'})}}>ลด ถอน รายวิชา</button></div>
-            <div><button className="btn btn-default" onClick={ () =>{ this.props.changeState({page:'ClassInformation'})}}>ดูตารางเรียน</button></div>
-            <div><button className="btn btn-default" onClick={ () =>{ this.props.changeState({page:'ExamInformation'})}}>ดูตารางสอบ</button></div>
-            <div><button className="btn btn-default" onClick={ () =>{ this.props.changeState({page:'GradeInformation'})}}>ดูเกรดรายวิชา</button></div>
-            <div><button className="btn btn-default" onClick={ () =>{ this.props.changeState({page:'DocumentRequest'})}}>ขอใบรับรอง</button></div>
-            <div><button className="btn btn-default" onClick={ () =>{ this.props.changeState({page:'RegistrationInformation'})}}>ตรวจสอบผลการลงทะเบียน</button></div>
-            <div><button className="btn btn-default" onClick={ () =>{ this.props.changeState({page:'CourseComment'})}}>ประเมินรายวิชา</button></div>
-            <div><button className="btn btn-default" onClick={ () => this.props.changeState({page:'SearchCourse'})}>ค้นหารายวิชา</button></div>
-            <LogoutButton changePage={this.props.changePage} />
+            <div className="btn-group-vertical btn-group-sm">
+                <button className="btn btn-default" onClick={ () =>{ this.props.changeState({page:'StudentInformation'})}}>ข้อมูลนักศึกษา</button>
+                <button className="btn btn-default" onClick={ () =>{ this.props.changeState({page:'RegisterCourse'})}}>ลงทะเบียนรายวิชา</button>
+                <button className="btn btn-default" onClick={ () =>{ this.props.changeState({page:'ManageCourse'})}}>ลด ถอน รายวิชา</button>
+                <button className="btn btn-default" onClick={ () =>{ this.props.changeState({page:'ClassInformation'})}}>ดูตารางเรียน</button>
+                <button className="btn btn-default" onClick={ () =>{ this.props.changeState({page:'ExamInformation'})}}>ดูตารางสอบ</button>
+                <button className="btn btn-default" onClick={ () =>{ this.props.changeState({page:'GradeInformation'})}}>ดูเกรดรายวิชา</button>
+                <button className="btn btn-default" onClick={ () =>{ this.props.changeState({page:'DocumentRequest'})}}>ขอใบรับรอง</button>
+                <button className="btn btn-default" onClick={ () =>{ this.props.changeState({page:'RegistrationInformation'})}}>ตรวจสอบผลการลงทะเบียน</button>
+                <button className="btn btn-default" onClick={ () =>{ this.props.changeState({page:'CourseComment'})}}>ประเมินรายวิชา</button>
+                <button className="btn btn-default" onClick={ () => this.props.changeState({page:'SearchCourse'})}>ค้นหารายวิชา</button>
+                <LogoutButton changePage={this.props.changePage} />
+            </div>
             </div>
         );
     }
