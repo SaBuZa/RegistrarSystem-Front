63010efb6c134e41951290c9ac21c967bc749c8e
Sarot Busala <sabuza97@gmail.com>
2018-04-30 23:12:45 +0700

    Clean button

--- src/Login/LoginTypeRadio/index.js
+++ src/Login/LoginTypeRadio/index.js
@@ -18,14 +18,15 @@ export default class LoginTypeRadio extends Component {
         //console.log(this.state.selectedType);
         //this.props.loginType = this.state.selectedType;
         //console.log(this.loginType);
-
+/*
+                <div className="radio-inline">
+                <label><input type="radio" name="typeRadio" value="Teacher" checked={this.state.selectedType==="Teacher"} onChange={this.handleTypeChange} />Teacher</label>
+                </div>*/
+                
         return (
             <div>
                 <div className="radio-inline">
                 <label><input type="radio" name="typeRadio" value="Student" checked={this.state.selectedType==="Student"} onChange={this.handleTypeChange} />Student</label>
-                </div>
-                <div className="radio-inline">
-                <label><input type="radio" name="typeRadio" value="Teacher" checked={this.state.selectedType==="Teacher"} onChange={this.handleTypeChange} />Teacher</label>
                 </div>
             </div>
             
